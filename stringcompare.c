f71159e988bb8f01eb15f2cd8ff95aef94049e44
Venkatesh Jaiswal <popcandy1x@gmail.com>
2022-03-21 10:27:16 +0530

    Added stringcompare.c

---
+++ stringcompare.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+int stringComparer(char a[],char b[],int c,int d)
+{
+	if(c==d)
+	{
+		for(int i=0;a[i]!=0;i++)
+		{
+			if(a[i]!=b[i])
+			{
+				return 0;
+			}
+		}
+	}
+	else
+	{
+		return 0;
+	}
+	return 1;
+}
+
+int main(){
+	char a[100];
+	printf("Enter the first string:");
+	scanf("%s",a);
+	int lengthfirststring=0;
+	for(int i=0;a[i]!='\0';i++)
+	{
+		lengthfirststring++;
+	}
+	char b[100];
+	printf("Enter the second string:");
+	scanf("%s",b);
+	int lengthsecondstring=0;
+	for(int i=0;a[i]!='\0';i++)
+	{
+		lengthsecondstring++;
+	}
+	if(stringComparer(a,b,lengthfirststring,lengthsecondstring))
+	{
+		printf("The strings are equal\n");
+	}
+	else
+	{
+		printf("The strings are not equal\n");
+	}
+	return 0;
+}
