3ed792bfade5c8d47396dca1e6b70d56c7ec4862
Venkatesh Jaiswal <popcandy1x@gmail.com>
2022-03-23 10:18:26 +0530

    Rectified stringreversepointers.c

--- stringreversepointers.c
+++ stringreversepointers.c
@@ -1,18 +1,27 @@
 #include<stdio.h>
 #include<string.h>
 
+void reverse(char a[],int n){
+	char *ptr,*ctr;
+	ptr=a;
+	ctr=&a[n-1];
+	char t;
+	for(int i=0;i<n/2;i++)
+	{
+		t=*(ptr);
+		*(ptr)=*(ctr);
+		*(ctr)=t;
+		ptr++;
+		ctr--;
+	}
+}
+
 int main(){
 	char a[100];
 	printf("Enter the string:");
 	scanf("%s",a);
 	int s=strlen(a);
-	char *ptr;
-	ptr=a+s-1;
-	printf("The string in reverse:");
-	for(int i=s-1;i>=0;i--)
-	{
-		printf("%c",*ptr);
-		ptr--;
-	}
+	reverse(a,s);
+	printf("The string in reverse:%s",a);
 	return 0;
 }
